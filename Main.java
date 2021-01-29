5ccf24ca145b675377f91c6536d2e4ee40b02cd6
Kwanyoung Christopher Kim <kwanyoung.kim@patelhs.org>
2021-01-29 13:47:20 +0000

    Created for loops which will loop through the score array and ask you for the scores via scanner. Then using another for loop, it will print out the results.

--- Main.java
+++ Main.java
@@ -12,15 +12,43 @@ class Main {
 
      // variables
     int teststoCompare = 5;
-    int testScores = new testScores[teststoCompare]
+    double[] testScores = new double[teststoCompare];
 
      // main code
-    System.out.print("\n");
+
+    newLine();
+
     System.out.println("Welcome to the 5TestAverager!");
+    newLine();
+
     sleep(1500);
-    System.out.println("In this machine, it will get the average of 5 of your grades, then give you whenever you passed the lowest overall grade!");
-    sleep(500);
+
+    System.out.println("In this machine, it will get the average of 5 of your grades (can be doubles), then give you whenever you passed the lowest overall grade!");
+    newLine();
+
+    sleep(3000);
+
     System.out.println("Have Fun!");
+    sleep(3000);
+
+    for (int i = 0; i < testScores.length; i++) {
+      newLine();
+      System.out.println("What is your score for assignment " + (i + 1) + "?");
+      testScores[i] = scan.nextDouble();
+    }
+
+    newLine();
+    System.out.println("Here are your scores:");
+
+    sleep(1500);
+
+    for (int i = 0; i < testScores.length; i++) {
+      System.out.println((i + 1) + " " + testScores[i]);
+    } 
+    
+    sleep(3000);
+
+    System.out.println("Getting your average...");
 
 
   }
@@ -33,12 +61,25 @@ class Main {
         Thread.interrupted();
       }
   }
+
+  static void newLine() {
+    System.out.print("\n");
+  }
 }
 
  // avg test score class
 class gettestscoreAvg {
    // avg test score method (Returns a double based on the average from your 5 test scores)
-  public double getAvg(int Test1, int Test2, int Test3, int Test4, int Test5) {
+  public double getAvg(int[] testscoreArray) {
+
+    return 2.2;
+  }
+}
+
+ // if passed class
+class gettestscoreAvg {
+   // if passed class method (Returns a boolean based on the average from your 5 test scores)
+  public double getAvg(int[] testscoreArray) {
 
     return 2.2;
   }
