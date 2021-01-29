99199efb1fca0c3df665f37c969c48ccb8b4705c
Kwanyoung Christopher Kim <kwanyoung.kim@patelhs.org>
2021-01-29 13:13:08 +0000

    Within the Main.java file, I created a new class and a new method, which currently doesn't do anything yet.

--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
-import java.util.Scanner;
-
-
 class Main {
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
 
 
     System.out.println("Hello world!");
   }
+}
 
-  public boolean gettestscores(int Test1, int Test2, int Test3, int Test4, int Test5) {
+class gettestscoreAvg {
+  public boolean gettestscoreAvg(int Test1, int Test2, int Test3, int Test4, int Test5) {
 
     return true;
   }
-
 }
