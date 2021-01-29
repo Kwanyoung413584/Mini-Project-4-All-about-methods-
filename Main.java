dcb0c65afc7a6ca600197111c6a88a49ea63c572
Kwanyoung Christopher Kim <kwanyoung.kim@patelhs.org>
2021-01-29 13:09:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+
+    System.out.println("Hello world!");
+  }
+
+  public boolean gettestscores(int Test1, int Test2, int Test3, int Test4, int Test5) {
+
+    return true;
+  }
+
+}
